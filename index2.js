75818826381179903f286a1b7c11828297d5791c
Anjolash <98609420+Anjolash@users.noreply.github.com>
2022-03-21 14:16:51 +0100

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,70 @@
+let isModalOpen = false;
+let isDarkMode = false;
+const scaleFactor = 1 / 20;
+
+function toggleModal(){
+    if (isModalOpen){
+        isModalOpen = false;        
+        return document.body.classList.remove("modal--open");
+        
+        
+    }
+    
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+    
+
+}
+
+function toggleBackground(){
+    if (isDarkMode){
+        isDarkMode= false;
+        return document.body.classList.remove("dark__mode")
+    }
+
+    isDarkMode = true;
+    document.body.classList += " dark__mode" 
+}
+
+
+function contact(event){
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading")
+    const success = document.querySelector(".modal__overlay--success")
+    loading.classList += " modal__overlay--visible";
+    emailjs
+    .sendForm(
+        "service_1vf4pz6",
+        "template_87ojzje",
+        event.target,
+        "oF4_sJqWlRQKkz_Xe"
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "omo nawa o"
+        )
+    });
+
+    
+}
+
+
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape")
+    const X = event.clientX * scaleFactor;
+    const Y = event.clientY * scaleFactor;
+
+
+    for(let i =0; i < shapes.length; i++){
+        const isOdd = i % 2 !== 0;
+        const isBool = isOdd ? -1 : 1;
+        shapes[i].style.transform = ` translate(${X * isBool}px, ${Y * isBool}px )`;
+        
+    }
+}
+
+
